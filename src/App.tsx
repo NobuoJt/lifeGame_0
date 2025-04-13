d346b48070557869a9e5b8bffb2b2b45ef49f3d1
nobuoJT <mario.oga0512@gmail.com>
2025-04-14 04:19:17 +0900

    +FEAT [tsx] 乱数による盤面生成

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
 import { useState } from 'react'
 import './App.css'
 
+/**再描画フラグ。ないと無限ループ。*/
+let table_mapped=false; 
+
+/** メイン。再描画時に呼ばれる */ 
 function App() {
 
-  const [cols,setCols]=useState(8)
-  const [rows,setRows]=useState(8)
+  const [cols,setCols]=useState(5)  //列col数の管理
+  const [rows,setRows]=useState(5)  //行row数の管理
 
-  return (
+  const [tableData,setTableData]=useState([[false]])  //盤面データの管理
+
+  if(table_mapped===false){   //盤面ランダム化
+
+    const colData:boolean[][]=[]  //列データ
+    let rowData:boolean[]=[]      //行データ
+    for(let c=0;c<cols;c++){
+      rowData=[]
+      for(let r=0;r<rows;r++){    
+        rowData.push(Math.random()>0.5) //乱数により生成
+      }
+      colData.push(rowData)
+    }
+    table_mapped=true
+    setTableData(colData)   //盤面更新
+}
+
+  return (  //DOM挿入
     <>
-      <div id="playArea">
+      <div id="playArea"> //盤面
         <table>
         {function(){
-          const col=[]
-          let row=[]
-            for(let i=0;i<cols;i++){
+
+          const col=[]  //列データ
+          let row=[]    //行データ
+
+            for(let c=0;c<cols;c++){//列ごと
               row=[]
-              for(let i=0;i<rows;i++){
-                row.push(
-                  <td>
-                    <button id="mainSwitch" ></button>
-                  </td>
-                )
+
+              for(let r=0;r<rows;r++){ //行ごと
+                if(tableData[c]!==undefined){//null参照回避
+                  row.push(//セルボタン設定
+                    <td>
+                      <button id="mainSwitch" 
+                              style={{backgroundColor:tableData[c][r]?'blue':'red'}}>
+
+                      </button>
+                    </td>
+                  )
+                }
               }
-              col.push(
+              col.push(//列データへ行データ押し込み
                 <tr>
                   {row}
                 </tr>
@@ -33,7 +62,8 @@ function App() {
         </table>
         
       </div>
-      <div id="menu">
+      
+      <div id="menu">   /** 下部設定エリア */
         <p>col count</p>
         <input type="number" value={cols} onChange={(e)=>setCols(Number(e.target.value))}></input>
         <p>row count</p>
