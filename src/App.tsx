f8dbcdee866d769c89133c482b9518a1a92a15fe
nobuoJT <mario.oga0512@gmail.com>
2025-04-14 21:30:08 +0900

    +FEAT [app,css] lifegameのフェーズ動作を仮実装(不完全)、テーブルカラー再定義 i

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ console.log(`version=(${packageJson.version})`)
 /** メイン。再描画時に呼ばれる */ 
 function App() {
 
-  const [col_length,setColLength]=useState(20)  //列col数の管理
-  const [row_length,setRowLength]=useState(35)  //行row数の管理
+  const [col_length,setColLength]=useState(6)  //列col数の管理
+  const [row_length,setRowLength]=useState(6)  //行row数の管理
 
   const [tableData,setTableData]=useState([[false]])  //盤面データの管理
 
@@ -26,7 +26,7 @@ function App() {
     for(let c=0;c<col_length;c++){
       row_data=[]
       for(let r=0;r<row_length;r++){    
-        row_data.push(Math.random()>0.5) //乱数により生成
+        row_data.push(Math.random()>1) //乱数により生成
       }
       col_data.push(row_data)
     }
@@ -51,7 +51,7 @@ function App() {
                   row_data.push(              //セルボタン設定
                     <td key={"r"+r}>
                       <button id="mainSwitch" 
-                              style={{backgroundColor:tableData[c][r]?'blue':'red'}}//tableDataに応じて青赤変化
+                              style={{backgroundColor:tableData[c][r]?'yellow':'midnightblue'}}//tableDataに応じて青赤変化
                               onClick={()=>{setTableData(
                                 tableData.map((col,c_ind)=>(  //行で走査
                                   c_ind==c?col.map(           //当該行・列で走査
@@ -87,13 +87,46 @@ function App() {
       </div>
     </>
   )
+
+/** プログラム領域 */
+
+function nextPhase(){
+  const new_table_data=tableData.map((col_data,ci)=>{ //行走査
+    return col_data.map((_row_data,ri)=>{              //列走査
+      if(tableData[ci][ri]){//当該セル生状態
+
+      /** 隣接セルがいきているか否か */
+      let live_count_near=0
+      if(ci<1           ?tableData[col_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
+      if(ci>1-col_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
+      if(ri<1           ?tableData[ci][row_length-1]:tableData[ci][ri-1]){live_count_near++}//左
+      if(ri>1-row_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
+        console.log("live",ci,ri,live_count_near)
+      if(live_count_near<=1){return false}  //過疎死
+      if(live_count_near==4){return false}  //過密死
+      return true //生存
+
+    }else{//当該セル死状態
+      /** 隣接セルがいきているか否か */
+      let live_count_near=0
+      if(ci<1           ?tableData[col_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
+      if(ci>1-col_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
+      if(ri<1           ?tableData[ci][row_length-1]:tableData[ci][ri-1]){live_count_near++}//左
+      if(ri>1-row_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
+        //console.log("death",ci,ri,live_count_near)
+      if(live_count_near==3){return true} //誕生
+      return false    //死亡状態継続
+    }
+      
+    })
+  })
+  setTableData(new_table_data)
+  return null
+}
+
+
 }
 
 export default App
 
 
-/** プログラム領域 */
-
-function nextPhase(){
-  return
-}
