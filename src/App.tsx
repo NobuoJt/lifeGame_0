b5e852a7a65c0081c532265dcad537fd158a49a1
nobuoJT <mario.oga0512@gmail.com>
2025-04-14 21:37:18 +0900

    *ref [app] 縦列col,y横行row逆に覚えていた。縦に並んでいるものそのものが列。列のカウントは横方向。

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,25 @@ console.log(`version=(${packageJson.version})`)
 /** メイン。再描画時に呼ばれる */ 
 function App() {
 
-  const [col_length,setColLength]=useState(6)  //列col数の管理
   const [row_length,setRowLength]=useState(6)  //行row数の管理
+  const [col_length,setColLength]=useState(6)  //列col数の管理
 
   const [tableData,setTableData]=useState([[false]])  //盤面データの管理
 
   if(table_mapped===false){   //盤面ランダム化
 
-    const col_data:boolean[][]=[]  //列データ
-    let   row_data:boolean[]=[]    //行データ
+    const row_data:boolean[][]=[]  //行データ
+    let   col_data:boolean[]=[]    //列データ
 
-    for(let c=0;c<col_length;c++){
-      row_data=[]
-      for(let r=0;r<row_length;r++){    
-        row_data.push(Math.random()>1) //乱数により生成
+    for(let c=0;c<row_length;c++){
+      col_data=[]
+      for(let r=0;r<col_length;r++){    
+        col_data.push(Math.random()>1) //乱数により生成
       }
-      col_data.push(row_data)
+      row_data.push(col_data)
     }
     table_mapped=true
-    setTableData(col_data)   //盤面更新
+    setTableData(row_data)   //盤面更新
 }
 
   return (  //DOM挿入
@@ -40,24 +40,24 @@ function App() {
         <table><tbody>
         {function(){
 
-          const col_data=[]  //列データ
-          let row_data=[]    //行データ
+          const row_data=[]  //行データ
+          let col_data=[]    //列データ
 
-            for(let c=0;c<col_length;c++){//列ごと
-              row_data=[]
+            for(let c=0;c<row_length;c++){//行ごと
+              col_data=[]
 
-              for(let r=0;r<row_length;r++){  //行ごと
+              for(let r=0;r<col_length;r++){  //列ごと
                 if(tableData[c]!==undefined){ //null参照回避
-                  row_data.push(              //セルボタン設定
+                  col_data.push(              //セルボタン設定
                     <td key={"r"+r}>
                       <button id="mainSwitch" 
                               style={{backgroundColor:tableData[c][r]?'yellow':'midnightblue'}}//tableDataに応じて青赤変化
                               onClick={()=>{setTableData(
-                                tableData.map((col,c_ind)=>(  //行で走査
-                                  c_ind==c?col.map(           //当該行・列で走査
-                                    (row,r_ind)=>r_ind==r     //当該列
-                                      ?!row:row               //反転・else非反転
-                                  ):col                       //else非反転
+                                tableData.map((row,c_ind)=>(  //列で走査
+                                  c_ind==c?row.map(           //当該列・行で走査
+                                    (col,r_ind)=>r_ind==r     //当該行
+                                      ?!col:col               //反転・else非反転
+                                  ):row                       //else非反転
                                 )))}}
                               >
                       </button>
@@ -65,23 +65,23 @@ function App() {
                   )
                 }
               }
-              col_data.push(  //列データへ行データ押し込み
+              row_data.push(  //行データへ列データ押し込み
                 <tr key={"c"+c}>
-                  {row_data}
+                  {col_data}
                 </tr>
               )
             }
-            return col_data   //行列データ
+            return row_data   //列行データ
         }()}
         </tbody></table>
         
       </div>
       
       <div id="menu">   {/** 下部設定エリア */}
-        <p>col count</p>
-        <input type="number" value={col_length} onChange={(e)=>{table_mapped=false;setColLength(Number(e.target.value))}}></input>
         <p>row count</p>
         <input type="number" value={row_length} onChange={(e)=>{table_mapped=false;setRowLength(Number(e.target.value))}}></input>
+        <p>col count</p>
+        <input type="number" value={col_length} onChange={(e)=>{table_mapped=false;setColLength(Number(e.target.value))}}></input>
         <p>phase change</p>
         <button onClick={()=>{nextPhase()}}>next phase</button>
       </div>
@@ -91,16 +91,16 @@ function App() {
 /** プログラム領域 */
 
 function nextPhase(){
-  const new_table_data=tableData.map((col_data,ci)=>{ //行走査
-    return col_data.map((_row_data,ri)=>{              //列走査
+  const new_table_data=tableData.map((row_data,ci)=>{ //列走査
+    return row_data.map((_col_data,ri)=>{              //行走査
       if(tableData[ci][ri]){//当該セル生状態
 
       /** 隣接セルがいきているか否か */
       let live_count_near=0
-      if(ci<1           ?tableData[col_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
-      if(ci>1-col_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
-      if(ri<1           ?tableData[ci][row_length-1]:tableData[ci][ri-1]){live_count_near++}//左
-      if(ri>1-row_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
+      if(ci<1           ?tableData[row_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
+      if(ci>1-row_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
+      if(ri<1           ?tableData[ci][col_length-1]:tableData[ci][ri-1]){live_count_near++}//左
+      if(ri>1-col_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
         console.log("live",ci,ri,live_count_near)
       if(live_count_near<=1){return false}  //過疎死
       if(live_count_near==4){return false}  //過密死
@@ -109,10 +109,10 @@ function nextPhase(){
     }else{//当該セル死状態
       /** 隣接セルがいきているか否か */
       let live_count_near=0
-      if(ci<1           ?tableData[col_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
-      if(ci>1-col_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
-      if(ri<1           ?tableData[ci][row_length-1]:tableData[ci][ri-1]){live_count_near++}//左
-      if(ri>1-row_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
+      if(ci<1           ?tableData[row_length-1][ri]:tableData[ci-1][ri]){live_count_near++}//上
+      if(ci>1-row_length?tableData[0]           [ri]:tableData[ci+1][ri]){live_count_near++}//下
+      if(ri<1           ?tableData[ci][col_length-1]:tableData[ci][ri-1]){live_count_near++}//左
+      if(ri>1-col_length?tableData[ci][0]           :tableData[ci][ri+1]){live_count_near++}//右
         //console.log("death",ci,ri,live_count_near)
       if(live_count_near==3){return true} //誕生
       return false    //死亡状態継続
