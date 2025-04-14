31278bde232ea03369b7e403178310433ff61eac
nobuoJT <mario.oga0512@gmail.com>
2025-04-14 19:00:29 +0900

    *ref [tsx] 命名規則に適合

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,25 @@ let table_mapped=false;
 /** メイン。再描画時に呼ばれる */ 
 function App() {
 
-  const [cols,setCols]=useState(20)  //列col数の管理
-  const [rows,setRows]=useState(35)  //行row数の管理
+  const [col_length,setColLength]=useState(20)  //列col数の管理
+  const [row_length,setRowLength]=useState(35)  //行row数の管理
 
   const [tableData,setTableData]=useState([[false]])  //盤面データの管理
 
   if(table_mapped===false){   //盤面ランダム化
 
-    const colData:boolean[][]=[]  //列データ
-    let rowData:boolean[]=[]      //行データ
-    for(let c=0;c<cols;c++){
-      rowData=[]
-      for(let r=0;r<rows;r++){    
-        rowData.push(Math.random()>0.5) //乱数により生成
+    const col_data:boolean[][]=[]  //列データ
+    let   row_data:boolean[]=[]    //行データ
+
+    for(let c=0;c<col_length;c++){
+      row_data=[]
+      for(let r=0;r<row_length;r++){    
+        row_data.push(Math.random()>0.5) //乱数により生成
       }
-      colData.push(rowData)
+      col_data.push(row_data)
     }
     table_mapped=true
-    setTableData(colData)   //盤面更新
+    setTableData(col_data)   //盤面更新
 }
 
   return (  //DOM挿入
@@ -36,10 +37,10 @@ function App() {
           const col=[]  //列データ
           let row=[]    //行データ
 
-            for(let c=0;c<cols;c++){//列ごと
+            for(let c=0;c<col_length;c++){//列ごと
               row=[]
 
-              for(let r=0;r<rows;r++){ //行ごと
+              for(let r=0;r<row_length;r++){ //行ごと
                 if(tableData[c]!==undefined){//null参照回避
                   row.push(//セルボタン設定
                     <td>
@@ -72,9 +73,11 @@ function App() {
       
       <div id="menu">   {/** 下部設定エリア */}
         <p>col count</p>
-        <input type="number" value={cols} onChange={(e)=>{table_mapped=false;setCols(Number(e.target.value))}}></input>
+        <input type="number" value={col_length} onChange={(e)=>{table_mapped=false;setColLength(Number(e.target.value))}}></input>
         <p>row count</p>
-        <input type="number" value={rows} onChange={(e)=>{table_mapped=false;setRows(Number(e.target.value))}}></input>
+        <input type="number" value={row_length} onChange={(e)=>{table_mapped=false;setRowLength(Number(e.target.value))}}></input>
+        <p>phase change</p>
+        <button onClick={()=>{next_phase()}}>next phase</button>
       </div>
     </>
   )
